2dcaa8193a0974e7c40a1ca559727203afc281f9
Pavel Goroshko <mcredpower@gmail.com>
2024-03-16 16:37:04 +0100

    comment out pug route

--- app/modules/template/index.js
+++ app/modules/template/index.js
@@ -1,16 +1,16 @@
 const Router = require("../../lib/router");
 
-const templateRouter = new Router("/template")
-  .get("/ejs", (req, res) =>
-    res.status(200).render("ejs.ejs", {
-      title: "ejs render",
-      body: "this page rendered with ejs template engine",
-    })
-  )
-  .get("/pug", (req, res) =>
-    res.status(200).render("pug.pug", {
-      body: "this page rendered with pug template engine",
-    })
-  );
+const templateRouter = new Router("/template").get("/ejs", (req, res) =>
+  res.status(200).render("ejs.ejs", {
+    title: "ejs render",
+    body: "this page rendered with ejs template engine",
+  })
+);
+// .get("/pug", (req, res) =>
+//   res.status(200).render("pug.pug", {
+//     title: "pug render",
+//     body: "this page rendered with pug template engine",
+//   })
+// );
 
 module.exports = templateRouter;
